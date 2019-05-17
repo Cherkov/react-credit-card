d61acc10bd62aed94e3fd86720364fbc46a281c3
Cherkov <kaccel1558@gmail.com>
2019-05-18 01:13:31 +0300

    Updates

---
+++ precache-manifest.9994c0b36948634503ac892c9a8f4af5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ee64e34c421d339d96a81368cf44f8",
+    "url": "/react-credit-card/index.html"
+  },
+  {
+    "revision": "ee3ead7aacd3d0f098ad",
+    "url": "/react-credit-card/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-credit-card/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ee3ead7aacd3d0f098ad",
+    "url": "/react-credit-card/static/js/main.fb6b655c.chunk.js"
+  },
+  {
+    "revision": "2b1e9e110329a4c3b295",
+    "url": "/react-credit-card/static/js/runtime~main.14ed8f70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-credit-card/static/media/logo.5d5d9eef.svg"
+  }
+]);
